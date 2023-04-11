a0b0767dab28e2a3604ccb122611bc28ab05baab
DianaDG <dianadiazg81@gmail.com>
2023-04-11 16:31:48 -0500

    Primera versión de código

---
+++ OIC_Ofertas/OIC_Ofertas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OIC_Ofertas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
